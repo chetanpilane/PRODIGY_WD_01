28a6def425ded87156e8a8e2fa108ab7eb764430
chetanpilane <140143075+chetanpilane@users.noreply.github.com>
2024-02-05 18:34:33 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,34 @@
+function bg()
+{
+    const p= document.getElementById('x');
+    const k= document.getElementById('a');
+      // Check if the user has scrolled beyond a certain point (e.g., 100 pixels)
+     p.style.color="green";
+     p.style.backgroundColor="orange";
+     
+
+}
+function ug()
+{
+    const k= document.getElementById('a');
+    // Check if the user has scrolled beyon
+    k.style.color="red";
+    k.style.backgroundColor="purple";
+
+}
+function mg()
+{
+    const u= document.getElementById('b');
+    // Check if the user has scrolled beyon
+    u.style.color="white";
+    u.style.backgroundColor="blue";
+
+}
+function tg()
+{
+    const s= document.getElementById('c');
+    // Check if the user has scrolled beyon
+    s.style.color="red";
+    s.style.backgroundColor="purple";
+
+}
